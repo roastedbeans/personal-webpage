c2276075470d4070dc6ea377aad89f6cdfad2a4c
Vince <vinceabella05@gmail.com>
2023-03-24 01:43:06 +0800

    fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Card } from "../components/Card";
 import { Footer } from "../components/Footer";
 
-import "../App.css";
-
 export const MainPage = () => {
   return (
     <>
