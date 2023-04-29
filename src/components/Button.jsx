269c8d087503a8d05d9c97755a9890e4ab0de1f6
Vince <vinceabella05@gmail.com>
2023-04-29 17:41:04 +0800

    update buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -11,27 +11,27 @@ export const ButtonList = () => {
 const Button = () => {
   return (
     <div className="flex item-center justify-center width: 50% height: 20px">
-      <a href="https://www.facebook.com/thevinchi05">
+      <a href="https://www.facebook.com/thevinchi05" target="_blank">
         <span className="logo fa-stack fa-2x">
           <i className="fab fa-facebook fa-stack-1x"></i>
         </span>
       </a>
-      <a href="https://www.instagram.com/vinchoutsidewalls">
+      <a href="https://www.instagram.com/vinchoutsidewalls" target="_blank">
         <span className="logo fa-stack fa-2x">
           <i className="fab fa-instagram fa-stack-1x"></i>
         </span>
       </a>
-      <a href="https://twitter.com/vinchi_05">
+      <a href="https://twitter.com/vinchi_05" target="_blank">
         <span className="logo fa-stack fa-2x">
           <i className="fab fa-twitter fa-stack-1x"></i>
         </span>
       </a>
-      <a href="https://www.linkedin.com/in/vinch05/">
+      <a href="https://www.linkedin.com/in/vinch05/" target="_blank">
         <span className="logo fa-stack fa-2x">
           <i className="fab fa-linkedin fa-stack-1x"></i>
         </span>
       </a>
-      <a href="https://github.com/roastedbeans">
+      <a href="https://github.com/roastedbeans" target="_blank">
         <span className="logo fa-stack fa-2x">
           <i className="fab fa-github fa-stack-1x"></i>
         </span>
